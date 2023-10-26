6da17aac812a08b9a9e33a9d5d704fb443ea079f
Maneet-K <maneetkaur797@gmail.com>
2023-10-26 13:16:09 +0530

    alignment

--- src/Pages/Feel.jsx
+++ src/Pages/Feel.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import NextComponent from './NextComponent';
+import "./Feel.css";
 
 function Feel() {
   const [name, setName] = useState("");
@@ -11,22 +12,30 @@ function Feel() {
   };
 
   return (
-    <div>
+    <div className='container' >
       {showNext ? (
         <NextComponent name={name} />
       ) : (
-        <div>
-          <h2>Enter Your Name</h2>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <button onClick={handleNextClick}>Next</button>
+        <div class="container center-card">
+          <div class="row">
+            <div class="col-12 d-flex align-items-center justify-content-center">
+              <div className="card" style={{ "width": "18rem", "box-shadow": "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" }}>
+                <div class="card-body">
+                  <h5 class="card-title">Enter Your Name</h5>
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  <button href="#" onClick={handleNextClick} className="btn btn-primary">Next</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      )}
-    </div>
-  );
+  )
+}
+</div>  );
 }
 
 export default Feel
