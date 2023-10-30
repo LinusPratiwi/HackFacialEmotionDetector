efdfeeb1be2e98cba88be1d2a547876772eb0428
LinusePratiwi <pratiwilinus@gmail.com>
2023-10-30 22:13:04 +0800

    build for prod

---
+++ build/precache-manifest.867dbc576fa4b988c73980a152a32628.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5ed94afe8cf8ee71cdd33401985b66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "536b00246c030a572259",
+    "url": "/static/css/2.24e085dd.chunk.css"
+  },
+  {
+    "revision": "33d710549daa6635955a",
+    "url": "/static/css/main.f994c5f7.chunk.css"
+  },
+  {
+    "revision": "536b00246c030a572259",
+    "url": "/static/js/2.d5f1861b.chunk.js"
+  },
+  {
+    "revision": "8a7080a45b7897f9495c8ddac6c52e4d",
+    "url": "/static/js/2.d5f1861b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d710549daa6635955a",
+    "url": "/static/js/main.e04f4801.chunk.js"
+  },
+  {
+    "revision": "8a38551fb80ba35d125c",
+    "url": "/static/js/runtime-main.b9bf97b2.js"
+  },
+  {
+    "revision": "5cf585fb2a38b17195dae89817db5b7f",
+    "url": "/static/media/hero-bg.5cf585fb.png"
+  }
+]);
