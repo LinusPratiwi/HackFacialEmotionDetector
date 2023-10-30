e2ec357997d5f4407b7829e1e84addbdd31e1dff
LinusePratiwi <pratiwilinus@gmail.com>
2023-10-30 22:53:24 +0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -26,53 +26,6 @@ function App() {
       </Navbar>
       <section id="content" className="row">
         <Outlet />
-
-        {/* <Row>
-          <div className="col-5 ps-5 align-self-center">
-            <h6 className="bls-text-subheadline mb-5">
-              Hi <span className="bls-text-subheadline text-decoration-underline">Lina,</span>
-            </h6>
-            <h1 className="bls-title-medium mb-3">Welcome to </h1>
-            <h1 className="bls-title-medium mb-5">
-              <img src="bliss_wl.png" className="d-inline-block align-top bls-image-span"></img>{' '}
-              Official Website!
-            </h1>
-          </div>
-          <div className="col-1 justify-content-center flex-column d-flex mt-4">
-            <img
-              className="bls-line-division my-3"
-              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAYAAACOEfKtAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAV0lEQVR4nO3QwQmAUAxEwRRlKjSVKbZhSvliBX5E8DIDe9jriwAAAAAAAAAAAAAAgBlZPazHVCwB+/uA8K+s88jq3X/pjresvfkAAAAAAAAAAAAAABDPLsV99fa/9fYgAAAAAElFTkSuQmCC"
-              alt="line-division"
-            />
-            <img
-              className="bls-line-division my-3"
-              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAYAAACOEfKtAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAV0lEQVR4nO3QwQmAUAxEwRRlKjSVKbZhSvliBX5E8DIDe9jriwAAAAAAAAAAAAAAgBlZPazHVCwB+/uA8K+s88jq3X/pjresvfkAAAAAAAAAAAAAABDPLsV99fa/9fYgAAAAAElFTkSuQmCC"
-              alt="line-division"
-            />
-            <img
-              className="bls-line-division my-3"
-              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAYAAACOEfKtAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAV0lEQVR4nO3QwQmAUAxEwRRlKjSVKbZhSvliBX5E8DIDe9jriwAAAAAAAAAAAAAAgBlZPazHVCwB+/uA8K+s88jq3X/pjresvfkAAAAAAAAAAAAAABDPLsV99fa/9fYgAAAAAElFTkSuQmCC"
-              alt="line-division"
-            />
-            <img
-              className="bls-line-division my-3"
-              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAYAAACOEfKtAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAV0lEQVR4nO3QwQmAUAxEwRRlKjSVKbZhSvliBX5E8DIDe9jriwAAAAAAAAAAAAAAgBlZPazHVCwB+/uA8K+s88jq3X/pjresvfkAAAAAAAAAAAAAABDPLsV99fa/9fYgAAAAAElFTkSuQmCC"
-              alt="line-division"
-            />
-            <img
-              className="bls-line-division my-3"
-              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAYAAACOEfKtAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAV0lEQVR4nO3QwQmAUAxEwRRlKjSVKbZhSvliBX5E8DIDe9jriwAAAAAAAAAAAAAAgBlZPazHVCwB+/uA8K+s88jq3X/pjresvfkAAAAAAAAAAAAAABDPLsV99fa/9fYgAAAAAElFTkSuQmCC"
-              alt="line-division"
-            />
-          </div>
-          <div className="col-6 align-self-center" style={{ paddingRight: '85px' }}>
-            
-            <div id="detail" className='mt-3'>
-              <Outlet />
-            </div>
-
-          </div>
-        </Row> */}
       </section>
     </div>
   )
